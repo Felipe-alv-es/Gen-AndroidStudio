66f433c7f82550d226742988a694142eb315c5a9
Felipe-alv-es <brago.sama@gmail.com>
2021-11-21 16:46:31 -0300

    Alterações de backGround e borda da tela principal

--- AplicationTestingLayout/app/src/main/java/com/example/aplicationtestinglayout/MainActivity.kt
+++ AplicationTestingLayout/app/src/main/java/com/example/aplicationtestinglayout/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.aplicationtestinglayout
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
